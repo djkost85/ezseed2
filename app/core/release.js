a98dfd176d77aa50d77d650bf1288a2b767fe7b1
soyuka <soyuka@gmail.com>
2013-12-20 11:01:53 +0100

    Allocine is back in da place

--- app/core/release.js
+++ app/core/release.js
@@ -253,5 +253,5 @@ var getAlbumInformations = function(album, cb) {
 
 module.exports.getAlbumInformations = getAlbumInformations;
 
-module.exports.getMovieInformations = require(__dirname + '/helpers/movies/themoviedb.js');
+module.exports.getMovieInformations = require(__dirname + '/helpers/movies/allocine.js');
 
